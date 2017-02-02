ccd4d1c06c1085a7eba207f411613e036b1364db
Oludemilade Mujib Raji <rajiano@FREE.local>
2017-02-02 13:46:45 -0800

    Initial Commit

---
+++ shakeMeSilly/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shakeMeSilly
+//
+//  Created by Oludemilade Mujib Raji on 2/2/17.
+//  Copyright © 2017 Oludemilade Mujib Raji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
