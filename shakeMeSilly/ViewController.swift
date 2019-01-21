d975e340c8f2dce6da7fa316e203571629160851
Oludemilade Raji <rajiolu@gmail.com>
2019-01-20 18:49:26 -0800

    Added functionality to shake count as device shakes.

--- shakeMeSilly/ViewController.swift
+++ shakeMeSilly/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var startRestartButton: UIButton!
+    @IBOutlet var imageView: UIImageView!
+    
+    var timer = Timer()
+    var countInt = 0
+    var scoreInt = 0
+    var modeInt = 0
+    var imageInt = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        startRestartButton.layer.cornerRadius = 8
+        countInt = 10
+        scoreInt = 0
+        timerLabel.text = String(countInt)
+        scoreLabel.text = String(scoreInt)
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startGame(_ sender: Any) {
+        if countInt == 10 {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ViewController.startCounter), userInfo: nil, repeats: true)
+            
+            startRestartButton.isEnabled = false
+            startRestartButton.alpha = 0.5
+            
+            modeInt = 1
+        }
+        
+        if countInt == 0 {
+            scoreInt = 0
+            countInt = 10
+            
+            startRestartButton.setTitle("Start Game", for: [])
+            timerLabel.text = String(countInt)
+            scoreLabel.text = String(scoreInt)
+        }
+    }
+    
+    func startCounter() {
+        countInt -= 1
+        timerLabel.text = String(countInt)
+        
+        if countInt == 0 {
+            timer.invalidate()
+            
+            startRestartButton.isEnabled = true
+            startRestartButton.alpha = 1.0
+            
+            startRestartButton.setTitle("Restart Game", for: [])
+            
+            modeInt = 0
+        }
+        
+    }
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            if modeInt == 1 {
+                scoreInt += 1
+                scoreLabel.text = String(scoreInt)
+                imageInt += 1
+                if imageInt > 4 {
+                    imageInt = 1
+                }
+                imageView.image = UIImage(named: "Maracas\(imageInt).png")
+            }
+            
+        }
+    }
 
 }
 
